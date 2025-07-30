673101c4ca533b29084c9cf4bc63e9c4c3c64243
Spazmann <csinclair@student.neumont.edu>
2025-07-29 23:16:29 -0600

    models done

--- Assets/Scripts/CreatureVisualMapping.cs
+++ Assets/Scripts/CreatureVisualMapping.cs
@@ -7,5 +7,5 @@ public class CreatureVisualMapping
     public string creatureName;
     public GameObject creaturePrefab;
     public CardArea creatureAreaType;
-    public Vector3 prefabScale = new Vector3(0.35f, 1f, 0.35f);
+    public Vector3 prefabScale = new Vector3(1f, 1f, 1f);
 }
